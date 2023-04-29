a3eec19428ea53b4e7aa5a1f0c867902388e0fef
M.T <maxime.torrelli@gmail.com>
2023-04-29 16:54:57 +0200

    Display dummy msg on all terminals. Work best on serial terminal

--- src/sessionMiddleware.ts
+++ src/sessionMiddleware.ts
@@ -1,26 +1,63 @@
 // import { Injectable } from '@angular/core'
 import { SessionMiddleware } from 'tabby-terminal'
+import { Logger } from 'tabby-core'
+
 
 // [dd/mm/yyyy hh:mm:ss:zzz]
 // const DateOverhead = 26
 
 // @Injectable()
-export class SerialTimestampSessionMiddleware extends SessionMiddleware {
-    // feedFromSession (data: Buffer): void {
-        // let date: Date = new Date();
-        // let newData: Buffer = Buffer.alloc(data.length + DateOverhead);
-        // newData = Buffer.concat([Buffer.from(date.toISOString(), "utf-8"), data])
-        // super.feedFromSession(newData)
-    // }
+export class SerialTimestampMiddleware extends SessionMiddleware {
+    private logger: Logger
+    private firstCall: Boolean
+
+    constructor (parentLogger: Logger) {
+        super()
+        this.firstCall = true
+        this.logger = parentLogger
+        this.logger.info(`Middleware created`)
+    }
+
 
     feedFromSession (data: Buffer): void {
-        for (const line of data.toString().split('\n')) {
-            const formatted = `[${new Date().toLocaleTimeString()}] ${line}\n`
-            this.outputToTerminal.next(Buffer.from(formatted))
+        this.logger.info(`data hex: ${data.toString('hex')}`)
+        this.logger.info(`data utf8: ${data.toString('utf8')}`)
+
+        if(this.firstCall === true) {
+            this.logger.info(`first call to feedFromSession`)
+            this.outputToTerminal.next(Buffer.from("[timestamp] "))
+            this.firstCall = false
+        }
+        // Original line for CR|LF: /\r|\n/.exec()
+        if(/\n/.exec(data.toString('utf8'))) {
+            this.logger.info(`data contains LF or CR`)
+            for (const line of data.toString('utf8').split('\n')) {
+                if(line.length > 0) {
+                    // let date: Date = new Date();
+                    // newData = Buffer.concat([Buffer.from(date.toISOString(), "utf-8"), data], data.length + DateOverhead)
+                    let newData = Buffer.concat([Buffer.from(line), Buffer.from("\n"), Buffer.from("[timestamp] ")])
+                    
+                    this.logger.info(`newData hex: ${newData.toString('hex')}`)
+                    this.logger.info(`newData utf8: ${newData.toString('utf8')}`)
+                    this.outputToTerminal.next(newData)
+                }
+            }
+        }
+        else {
+            // this.logger.info(`data DOESN'T contain LF`)
+            this.outputToTerminal.next(data)
         }
     }
 
+    // feedFromSession (data: Buffer): void {
+    //     for (const line of data.toString().split('\n')) {
+    //         const formatted = `[${new Date().toLocaleTimeString()}] ${line}\n`
+    //         this.outputToTerminal.next(Buffer.from(formatted))
+    //     }
+    // }
+
     close (): void {
-        // super.close()
+        this.logger.info(`Middleware closing`)
+        super.close()
     }
 }
--- src/terminalDecorator.ts
+++ src/terminalDecorator.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core'
 // import { bufferTime } from 'rxjs'
-import { TerminalDecorator, BaseTerminalTabComponent, BaseSession, SessionMiddleware} from 'tabby-terminal'
-import { SerialTimestampSessionMiddleware } from './sessionMiddleware'
+import { TerminalDecorator, BaseTerminalTabComponent } from 'tabby-terminal'
+import { Logger, LogService } from 'tabby-core'
+import { SerialTimestampMiddleware } from './sessionMiddleware'
 
 @Injectable()
 export class SerialTimestampDecorator extends TerminalDecorator {
-    constructor (
-        ) {
+    logger: Logger
+
+    constructor (log: LogService) {
         super()
+        this.logger = log.create('serial-timestamp')  
     }
 
-    attach (terminal: BaseTerminalTabComponent): void {
+    attach (terminal: BaseTerminalTabComponent<any>): void {
         // tab.input$.pipe(bufferTime(3000)).subscribe((buffers: Buffer[])  => {
             // if (Buffer.concat(buffers).toString().includes('ls\r')) {
             //     this.clippy.speak('It looks like you\'re using the "ls" command. Did you know that you can use it to list files?')
@@ -26,12 +29,21 @@ export class SerialTimestampDecorator extends TerminalDecorator {
         }
     }
 
-    private attachToSession (terminal: BaseTerminalTabComponent) {
+    detach (terminal: BaseTerminalTabComponent<any>): void {
+        this.logger.info(`Detaching from session: ${terminal.title}`)
+    }
+
+    private attachToSession (terminal: BaseTerminalTabComponent<any>) {
         // session.output$.subscribe(data => {
         //     if (data.includes('command not found')) {
         //         this.clippy.speak('It looks like you\'ve typed in an incorrect command. Consider typing in a correct command instead.')
         //     }
         // })
-        terminal.session.middleware.push(new SerialTimestampSessionMiddleware())
+        if (!terminal.session) {
+            this.logger.info(`Session is empty !`)
+            return
+        }
+        this.logger.info(`Attaching to session: ${terminal.title}`)
+        terminal.session.middleware.push(new SerialTimestampMiddleware(this.logger))
     }
 }
