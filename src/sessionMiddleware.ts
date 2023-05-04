fb46b31d959a8072b9ba88b449902f7a76da27fe
M.T <maxime.torrelli@gmail.com>
2023-05-04 17:34:10 +0200

    Refactor mechanism to add timestamp

--- src/sessionMiddleware.ts
+++ src/sessionMiddleware.ts
@@ -14,16 +14,16 @@ const US = 'mm/dd/yyyy hh:mm:ss.SSS'
 // @Injectable()
 export class SerialTimestampMiddleware extends SessionMiddleware {
     private logger: Logger
-    private firstCall: Boolean
+    private startOfLine: Boolean
     private formatChanged: Boolean
     private currentTimestampFormat: string
 
-    constructor (
+    constructor(
         private config: ConfigService,
         parentLogger: Logger
     ) {
         super()
-        this.firstCall = true
+        this.startOfLine = true
         this.formatChanged = false
         this.currentTimestampFormat = this.config.store.serialTimestampPlugin.timestamp
 
@@ -37,112 +37,109 @@ export class SerialTimestampMiddleware extends SessionMiddleware {
     }
 
 
-    feedFromSession (data: Buffer): void {
+    feedFromSession(data: Buffer): void {
         let currentDate: String
         let newData: Buffer
+        let occurences: number
+        let line: String
+        let strRemaining: String
 
         // this.logger.info(`data hex: ${data.toString('hex')}`)
         // this.logger.info(`data utf8: ${data.toString('utf8')}`)
 
-        if(this.firstCall === true) {
-            // this.logger.info(`first call to feedFromSession`)
+        currentDate = this.getDate()
 
-            currentDate = this.getDate()
-
-            if(currentDate !== "") {
-                newData = Buffer.concat([Buffer.from("["), Buffer.from(currentDate), Buffer.from("] ")])
-                this.outputToTerminal.next(newData)
-            }
-
-            this.firstCall = false
-        }
-        else if(this.formatChanged === true)
-        {
-            // this.logger.info(`first call to feedFromSession after timestamp format changed`)
+        if (currentDate !== "") {
+            // split always return an empty string if it did not found the required string/char
+            // occurences is set to 1 when LF was not found to force the for loop once
+            occurences = data.toString('utf8').split('\n').length > 1 ?data.toString('utf8').split('\n').length-1 : data.toString('utf8').split('\n').length
+            strRemaining = data.toString('utf8')
             
-            currentDate = this.getDate()
+            for (let i = 0; i < occurences; i++) {
+                // Original line for CR|LF: /\r|\n/.exec()
+                if (/\n/.exec(strRemaining.toString())) {
+                    line = strRemaining.substring(0, strRemaining.indexOf("\n") + 1)
+                    strRemaining = strRemaining.substring(strRemaining.indexOf("\n") + 1)
+                }
+                else {
+                    line = strRemaining
+                    strRemaining = null
+                }
 
-            if(currentDate !== "") {
-                // Add a line feed to allow new data to be displaed without any date in front
-                newData = Buffer.concat([Buffer.from("\r\n"), Buffer.from("["), Buffer.from(currentDate), Buffer.from("] ")])
-                this.outputToTerminal.next(newData)
-            }
-            else
-            {
-                // Add a line feed to allow new data to be displaed without any date in front
-                this.outputToTerminal.next(Buffer.from("\r\n"))
-            }
+                if (this.startOfLine === true || this.formatChanged === true) {
+                    newData = Buffer.concat([Buffer.from("["), Buffer.from(currentDate), Buffer.from("] "), Buffer.from(line)])
 
-            this.formatChanged = false
-        }
+                    this.outputToTerminal.next(newData)
 
-        // Original line for CR|LF: /\r|\n/.exec()
-        if(/\n/.exec(data.toString('utf8'))) {
-            // this.logger.info(`data contains LF or CR`)
-            
-            currentDate = this.getDate()
+                    if (this.startOfLine === true) {
+                        this.startOfLine = false
+                    }
 
-            if(currentDate !== "") {
-                for (const line of data.toString('utf8').split('\n')) {
-                    if(line.length > 0) {
-                        newData = Buffer.concat([Buffer.from(line), Buffer.from("\n"), Buffer.from("["), Buffer.from(currentDate), Buffer.from("] ")])
-                        
-                        // this.logger.info(`newData hex: ${newData.toString('hex')}`)
-                        // this.logger.info(`newData utf8: ${newData.toString('utf8')}`)
-                        
-                        this.outputToTerminal.next(newData)
+                    if (this.formatChanged === true) {
+                        this.formatChanged = false
                     }
                 }
-            }
-            else
-            {
-                // this.logger.info(`no date to prepend`)
-                this.outputToTerminal.next(data)
+                else {
+                    this.outputToTerminal.next(Buffer.from(line))
+                }
+
+                if (/\n/.exec(line.toString())) {
+                    this.startOfLine = true
+                }
+
+                // Last string does not contains \n, thus, must be output because for loop will not
+                // reach it
+                if (strRemaining && strRemaining.length > 0 && /\n/.exec(strRemaining.toString()) === null) {
+                    newData = Buffer.concat([Buffer.from("["), Buffer.from(currentDate), Buffer.from("] "), Buffer.from(strRemaining)])
+
+                    this.outputToTerminal.next(newData)
+
+                    this.startOfLine = false
+                }
             }
         }
-        else {
-            // this.logger.info(`data DOESN'T contain LF`)
+        else { // Timestamp format is "None"
             this.outputToTerminal.next(data)
         }
     }
 
-    close (): void {
+    close(): void {
         // this.logger.info(`Middleware closing`)
         super.close()
     }
 
-    private updateTimestampFormat () {
+    private updateTimestampFormat() {
         this.currentTimestampFormat = this.config.store.serialTimestampPlugin.timestamp
         this.formatChanged = true // set to true to make sur the date is added before the new data
         this.logger.info(`Serial timestamp changed to ${this.currentTimestampFormat}`)
     }
 
-    private getDate (): String {
+    private getDate(): String {
         let sDate: String = new String("")
         let date: Date
 
-        switch(this.currentTimestampFormat) {
+        switch (this.currentTimestampFormat) {
             case None:
                 sDate = ""
                 break;
             case ISO:
-                date =  new Date()
+                date = new Date()
                 sDate = date.toISOString()
                 break;
             case Europe:
                 date = new Date()
 
-                sDate = sDate.concat(date.getDate().toString(10).padStart(2, "0"), "/", (date.getMonth()+1).toString(10).padStart(2, "0"), "/", date.getFullYear().toString(10), " ",
-                                        date.getHours().toString(10).padStart(2, "0"), ":", date.getMinutes().toString(10).padStart(2, "0"), ":", date.getSeconds().toString(10).padStart(2, "0"), ".", date.getMilliseconds().toString(10).padStart(3, "0"))
-                
+                sDate = sDate.concat(date.getDate().toString(10).padStart(2, "0"), "/", (date.getMonth() + 1).toString(10).padStart(2, "0"), "/", date.getFullYear().toString(10), " ",
+                    date.getHours().toString(10).padStart(2, "0"), ":", date.getMinutes().toString(10).padStart(2, "0"), ":", date.getSeconds().toString(10).padStart(2, "0"), ".", date.getMilliseconds().toString(10).padStart(3, "0"))
+
                 // this.logger.info(`Date created ${sDate}`)
                 break;
             case US:
                 date = new Date()
 
-                sDate = sDate.concat((date.getMonth()+1).toString(10).padStart(2, "0"), "/", date.getDate().toString(10).padStart(2, "0"), "/", date.getFullYear().toString(10), " ",
-                                        date.getHours().toString(10).padStart(2, "0"), ":", date.getMinutes().toString(10).padStart(2, "0"), ":", date.getSeconds().toString(10).padStart(2, "0"), ".", date.getMilliseconds().toString(10).padStart(3, "0"))
-                
+                sDate = sDate.concat((date.getMonth() + 1).toString(10).padStart(2, "0"), "/", date.getDate().toString(10).padStart(2, "0"), "/", date.getFullYear().toString(10), " ",
+                    date.getHours().toString(10).padStart(2, "0"), ":", date.getMinutes().toString(10).padStart(2, "0"), ":", date.getSeconds().toString(10).padStart(2, "0"), ".", date.getMilliseconds().toString(10).padStart(3, "0"))
+
                 break;
             default:
                 this.logger.error(`Unknown format ${this.currentTimestampFormat}`)
