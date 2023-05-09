1330972b42dc88229191791755b772b40fcabcc4
M.T <maxime.torrelli@gmail.com>
2023-05-09 16:42:31 +0200

    Correct issue related to CR not followed by LF

--- src/sessionMiddleware.ts
+++ src/sessionMiddleware.ts
@@ -43,64 +43,112 @@ export class SerialTimestampMiddleware extends SessionMiddleware {
         let occurences: number
         let line: String
         let strRemaining: String
+        // let str: String
+
+        // this.logger.info(`-------------------------- Entering feedFromSession --------------------------`)
 
         // this.logger.info(`data hex: ${data.toString('hex')}`)
         // this.logger.info(`data utf8: ${data.toString('utf8')}`)
+
+        // str = 'Provided data '
+        // data.forEach((char) => {
+        //     str = str + '0x' + char.toString(16) + ' '
+        // })
+
+        // this.logger.info(`Provided string ${data.toString('utf8')}`)
+        // this.logger.info(`${str}`)
 
         currentDate = this.getDate()
 
         if (currentDate !== "") {
             // split always return an empty string if it did not found the required string/char
             // occurences is set to 1 when LF was not found to force the for loop once
-            occurences = data.toString('utf8').split('\n').length > 1 ?data.toString('utf8').split('\n').length-1 : data.toString('utf8').split('\n').length
+            occurences = data.toString('utf8').split('\n').length > 1 ? data.toString('utf8').split('\n').length-1 : data.toString('utf8').split('\n').length
             strRemaining = data.toString('utf8')
             
+            // this.logger.info(`occurences ${occurences}`)
+            // this.logger.info(`data.toString('utf8').split(CRLF OR LF).length ${data.toString('utf8').split(/\r\n|\n/).length}`)
+            // this.logger.info(`data.toString('utf8').length ${data.toString('utf8').length}`)
+
             for (let i = 0; i < occurences; i++) {
+                // this.logger.info(`Loop number ${i}`)
                 // Original line for CR|LF: /\r|\n/.exec()
                 if (/\n/.exec(strRemaining.toString())) {
+                    // this.logger.info(`Remaining data contains LF`)
                     line = strRemaining.substring(0, strRemaining.indexOf("\n") + 1)
                     strRemaining = strRemaining.substring(strRemaining.indexOf("\n") + 1)
                 }
                 else {
+                    // this.logger.info(`Remaining data does not contain LF`)
                     line = strRemaining
                     strRemaining = null
                 }
 
+                // this.logger.info(`line ${line}`)
+                // str = 'line data '
+                // Buffer.from(line, 'utf8').forEach((char) => {
+                //     str = str + '0x' + char.toString(16) + ' '
+                // })
+                // this.logger.info(`${str}`)
+
+                // if(strRemaining) {
+                //     this.logger.info(`strRemaining ${strRemaining}`)
+                //     str = 'strRemaining data '
+                //     Buffer.from(strRemaining, 'utf8').forEach((char) => {
+                //         str = str + '0x' + char.toString(16) + ' '
+                //     })
+                //     this.logger.info(`${str}`)
+                // }
+
                 if (this.startOfLine === true || this.formatChanged === true) {
+                    // this.logger.info(`line is the beginning of the line thus prepend the date`)
                     newData = Buffer.concat([Buffer.from("["), Buffer.from(currentDate), Buffer.from("] "), Buffer.from(line)])
 
                     this.outputToTerminal.next(newData)
 
                     if (this.startOfLine === true) {
+                        // this.logger.info(`Unsetting startOfLine`)
                         this.startOfLine = false
                     }
 
                     if (this.formatChanged === true) {
+                        // this.logger.info(`Unsetting formatChanged`)
                         this.formatChanged = false
                     }
                 }
                 else {
+                    // this.logger.info(`line is not the beginning of the line. Do not  prepend the date`)
                     this.outputToTerminal.next(Buffer.from(line))
                 }
 
-                if (/\n/.exec(line.toString())) {
+                // If line contains \n or if it ends with \r
+                if (/\n/.exec(line.toString()) || line.charAt(line.length - 1) === '\r') {
+                    // this.logger.info(`line contains LF thus setting startOfLine`)
                     this.startOfLine = true
                 }
 
                 // Last string does not contains \n, thus, must be output because for loop will not
                 // reach it
                 if (strRemaining && strRemaining.length > 0 && /\n/.exec(strRemaining.toString()) === null) {
+                    // this.logger.info(`Last string does not contains LF`)
                     newData = Buffer.concat([Buffer.from("["), Buffer.from(currentDate), Buffer.from("] "), Buffer.from(strRemaining)])
 
                     this.outputToTerminal.next(newData)
-
-                    this.startOfLine = false
+                    
+                    if (strRemaining.charAt(strRemaining.length - 1) === '\r') {
+                        this.startOfLine = true
+                    }
+                    else {
+                        this.startOfLine = false
+                    }
                 }
             }
         }
         else { // Timestamp format is "None"
             this.outputToTerminal.next(data)
         }
+
+        // this.logger.info(`-------------------------- Leaving feedFromSession --------------------------`)
     }
 
     close(): void {
